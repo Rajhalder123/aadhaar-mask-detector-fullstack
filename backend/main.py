f8d18d6bcfe1afe25ec879a3da6b49e0a56737fe
Raj Halder <rajhalder8000@gmail.com>
2026-02-28 22:35:02 +0530

    Allow all CORS origins to fix Vercel branch deployment blocks

--- backend/main.py
+++ backend/main.py
@@ -24,11 +24,7 @@ app = FastAPI()
 # =========================
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:3000",
-        "http://localhost:3000",
-        "https://aadhaar-mask-detector.vercel.app"
-    ],  # or use ["*"] for testing
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
